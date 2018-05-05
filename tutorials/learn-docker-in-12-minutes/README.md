1acf19b82e2a7efe297a2de1282036a2be16c5ea
atrox3d <atrox3d@gmail.com>
2018-05-05 09:38:46 +0200

    docker-toolbox for windows workarounds

--- tutorials/learn-docker-in-12-minutes/README.md
+++ tutorials/learn-docker-in-12-minutes/README.md
@@ -3,3 +3,15 @@
 
 source: https://youtu.be/YFl2mCHdv24
 
+# workarounds per Windows/Docker toolbox/GitBash
+- usare la sintassi :
+```
+wimpty docker ...
+```
+
+per errore:
+```
+the input device is not a TTY.  If you are using mintty, try prefixing the command with 'winpty'
+```
+https://stackoverflow.com/a/39858122
+https://github.com/docker/toolbox/issues/607 
--- tutorials/learn-docker-in-12-minutes/start.sh
+++ tutorials/learn-docker-in-12-minutes/start.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-docker run -p80:80 -v $(readlink -e src):/var/www/html/ hellophp
-#docker run -p80:80 hellophp
+IMAGENAME=hellophp
+CONTAINERNAME=${IMAGENAME}
+HOSTPORT=80
 
+#Windows/docker-toolbox/gitbash?
+which winpty 2> /dev/null && {
+	#https://stackoverflow.com/a/39858122
+	WINPTY=winpty
+	#https://github.com/docker/toolbox/issues/607
+	MOUNT_PREFIX=/
+} || {
+	WINPTY=""
+	MOUNT_PREFIX=""
+}
+
+${WINPTY} docker run -d --rm -p${HOSTPORT}:80 -v ${MOUNT_PREFIX}$(readlink -e src):/var/www/html/ hellophp
+#docker run -p80:80 hellophp
