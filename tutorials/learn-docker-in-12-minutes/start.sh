e1de1a65d7dda2260c25427d029cbb516d9cac27
atrox3d <atrox3d@gmail.com>
2018-05-05 10:38:33 +0200

    improve shell

--- tutorials/learn-docker-in-12-minutes/start.sh
+++ tutorials/learn-docker-in-12-minutes/start.sh
@@ -4,7 +4,7 @@ CONTAINERNAME=${IMAGENAME}
 HOSTPORT=80
 
 #Windows/docker-toolbox/gitbash?
-which winpty 2> /dev/null && {
+which winpty 2>&1 > /dev/null && {
 	#https://stackoverflow.com/a/39858122
 	WINPTY=winpty
 	#https://github.com/docker/toolbox/issues/607
@@ -15,4 +15,10 @@ which winpty 2> /dev/null && {
 }
 
 ${WINPTY} docker run -d --rm -p${HOSTPORT}:80 -v ${MOUNT_PREFIX}$(readlink -e src):/var/www/html/ ${IMAGENAME}
+[ $? -eq 0 ] && {
+	ID=$(docker ps -lq)
+	echo "stop   : docker stop $ID"
+	echo "attach : docker attach $ID"
+	echo "logs   : docker logs $ID"
+}
 #docker run -p80:80 hellophp
