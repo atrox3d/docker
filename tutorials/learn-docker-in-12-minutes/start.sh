7a14e173a7b3fbb4ef543671438f47183aa93111
atrox3d <atrox3d@gmail.com>
2018-05-05 10:47:07 +0200

    improve shell

--- tutorials/learn-docker-in-12-minutes/start.sh
+++ tutorials/learn-docker-in-12-minutes/start.sh
@@ -5,6 +5,8 @@ HOSTPORT=80
 
 #Windows/docker-toolbox/gitbash?
 which winpty 2>&1 > /dev/null && {
+	_winpty=$(which winpty)
+	echo "detected $_winpty : setting WINPTY and MOUNT_PREFIX variables"
 	#https://stackoverflow.com/a/39858122
 	WINPTY=winpty
 	#https://github.com/docker/toolbox/issues/607
@@ -13,12 +15,19 @@ which winpty 2>&1 > /dev/null && {
 	WINPTY=""
 	MOUNT_PREFIX=""
 }
-
-${WINPTY} docker run -d --rm -p${HOSTPORT}:80 -v ${MOUNT_PREFIX}$(readlink -e src):/var/www/html/ ${IMAGENAME}
+_cmmdline="${WINPTY} docker run -d --rm -p${HOSTPORT}:80 -v ${MOUNT_PREFIX}$(readlink -e src):/var/www/html/ ${IMAGENAME}"
+echo "[RUNNING] :"
+echo "-------------------------------------------------------------------------"
+echo "${_cmmdline}"
+echo "-------------------------------------------------------------------------"
+${_cmmdline}
 [ $? -eq 0 ] && {
+	echo "[SUCCESS]"
 	ID=$(docker ps -lq)
 	echo "stop   : docker stop $ID"
 	echo "attach : docker attach $ID"
 	echo "logs   : docker logs $ID"
+} || {
+	echo "[FAILURE ]with exit code $?"
 }
 #docker run -p80:80 hellophp
