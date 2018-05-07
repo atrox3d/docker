2e5c67a5749f789b912ff8224a07fe1c0813158b
atrox3d <atrox3d@gmail.com>
2018-05-07 14:27:39 +0200

    improve build.sh

---
+++ dockerfile/ENTRYPOINT/ubuntu-top-b.exec.form/README.md
@@ -0,0 +1,24 @@
+
+# ENTRYPOINT exec form
+
+
+**per eseguire override di ENTRYPOINT e  CMD usare il flag ```--entrypoint```, specificando eventuali argomenti DOPO il nome dell'immagine**
+ 
+ad esempio:
+ 
+```
+docker run -it --rm --entrypoint /bin/bash <NOMEIMMAGINE>
+docker run -it --rm --entrypoint /bin/bash <NOMEIMMAGINE> -c "ls -l"
+```
+**NOTA per git bash windows: a causa di MINGW64 gli slash vanno raddoppiati:**
+https://stackoverflow.com/a/39858122
+```
+winpty docker run -it --rm --entrypoint //bin//bash <NOMEIMMAGINE>
+winpty docker run -it --rm --entrypoint //bin//bash <NOMEIMMAGINE> -c "ls -l"
+```
+
+**Non è possibile eseguire l'override dei soli argomenti perchè nel docker file gli argomenti sono specificati direttamente in ENTRYPOINT:**
+
+```
+ENTRYPOINT [ "top", "-b" ]
+```
--- dockerfile/ENTRYPOINT/ubuntu-top.exec.form/build.sh
+++ dockerfile/ENTRYPOINT/ubuntu-top.exec.form/build.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
-IMAGENAME=ubuntutop-exec
-docker build -t ${IMAGENAME} .
+
+function log()
+{
+	echo "[$(basename $0)] ${@}"
+}
+
+log "check for build.properties..."
+[ -s build.properties ] && {
+	log "list build properties:"
+	
+	while read _line
+	do
+		log "$_line"
+	done < build.properties
+	
+	log "sourcing build.properties"
+	. ./build.properties
+}
+
+[ $# -ge 1 ] && {
+	log "setting IMAGENAME from argument '$1'"
+	IMAGENAME="${1}"
+}
+
+[ ${IMAGENAME:-ERR_DOCKER_IMAGENAME_UNSET} = ERR_DOCKER_IMAGENAME_UNSET ] && {
+	log " [ERROR] variable IMAGENAME is unset"
+	log " [ERROR] check file build.properties or pass IMAGENAME value as argument \$1"
+	exit 1
+}
+
+_cmdline="docker build -t ${IMAGENAME} ."
+log "$_cmdline"
+docker build -t ${IMAGENAME} . && {
+	log "[SUCCESS] image ${IMAGENAME} built succesfully"
+} || {
+	log "[FAIL] error building image ${IMAGENAME}"
+}
