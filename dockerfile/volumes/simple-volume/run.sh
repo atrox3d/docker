c91c9e8b4049ddc4812e46f051adce194a29fbd3
atrox3d <atrox3d@gmail.com>
2018-05-07 18:47:41 +0200

    refactoring: move simple-volume

--- dockerfile/volumes/simple-volume/run.sh
+++ dockerfile/volumes/simple-volume/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 . ../../../configure-environment.sh || echo "ERROR configuring environment"
 
+HOST_PATH=${PWD}/logs
+CONTAINER_PATH=/logs
 ${WINPTY} docker run --rm \
--v ${MOUNT_PREFIX}${PWD}/logs:/logs ubuntu \
+-v ${MOUNT_PREFIX}${HOST_PATH}:${CONTAINER_PATH} ubuntu \
 $( [ "$WINPTY" = "" ] && echo /bin/bash || echo //bin//bash ) -c \
 "ls -l logs/*;rm logs/from-container;ls -l logs/*;touch logs/from-container;ls -l logs/*"
