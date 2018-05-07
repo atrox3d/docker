244065b53d461bbfd4722731df1261fa6c238ef3
atrox3d <atrox3d@gmail.com>
2018-05-07 18:10:07 +0200

    big refactoring: fix docker-tools double slash

--- dockerfile/volumes/run.sh
+++ dockerfile/volumes/run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-winpty docker run -v //c/Users/lombardo/Dropbox/dev/github/atrox3d/docker/dockerfile/volumes/logs:/logs ubuntu //bin//bash -c "ls -l logs/*;rm logs/from-container;ls -l logs/*;touch logs/from-container;ls -l logs/*"
+. ../../configure-environment.sh || echo "ERROR configuring environment"
+
+${WINPTY} docker run --rm \
+-v ${MOUNT_PREFIX}${PWD}/logs:/logs ubuntu \
+$( [ "$WINPTY" = "" ] && echo /bin/bash || echo //bin//bash ) -c \
+"ls -l logs/*;rm logs/from-container;ls -l logs/*;touch logs/from-container;ls -l logs/*"
