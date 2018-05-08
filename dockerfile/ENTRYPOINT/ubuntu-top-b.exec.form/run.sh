a1bb89201d8f7206788475c8c02708aae9228f59
atrox3d <atrox3d@gmail.com>
2018-05-08 08:14:41 +0200

    finish refactor

--- dockerfile/ENTRYPOINT/ubuntu-top-b.exec.form/run.sh
+++ dockerfile/ENTRYPOINT/ubuntu-top-b.exec.form/run.sh
@@ -3,16 +3,15 @@
 . "${DOCKER_HELPERS}"/setimagename.sh
 #IMAGENAME=ubuntutop-exec-b
 CONTAINERNAME=${IMAGENAME}
-#which winpty 2> /dev/null && WINPTY=winpty || WINPTY=""
-
+_BASH="$( [ "$WINPTY" = "" ] && echo /bin/bash || echo //bin//bash )"
 
 cat <<EOF
 # esecuzione con parametri CMD ed ENTRYPOINT da dockerfile
 ${WINPTY} docker run -it --rm --name ${CONTAINERNAME} ${IMAGENAME}
 
 # esecuzione con override ENTRYPOINT e parametri CMD
-${WINPTY} docker run -it --rm --name ${CONTAINERNAME} --entrypoint $( [ "$WINPTY" = "" ] && echo /bin/bash || echo //bin//bash ) ${IMAGENAME}
-${WINPTY} docker run -it --rm --name ${CONTAINERNAME} --entrypoint $( [ "$WINPTY" = "" ] && echo /bin/bash || echo //bin//bash ) ${IMAGENAME} -c "ls -l"
+${WINPTY} docker run -it --rm --name ${CONTAINERNAME} --entrypoint ${_BASH} ${IMAGENAME}
+${WINPTY} docker run -it --rm --name ${CONTAINERNAME} --entrypoint ${_BASH} ${IMAGENAME} -c "ls -l"
 
 # esecuzione con override parametri CMD
 ${WINPTY} docker run -it --rm <NOMEIMMAGINE> ""
--- dockerfile/ENTRYPOINT/ubuntu-top.exec.form/build.sh
+++ dockerfile/ENTRYPOINT/ubuntu-top.exec.form/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-. ../../../.helpers/build.sh "$@"
+. ../../../configure-environment.sh || echo "ERROR configuring environment"
+. "${DOCKER_HELPERS}"/build.sh "$@"
+
