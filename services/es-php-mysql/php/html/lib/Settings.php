412b9e4b13f22f8004833f4f877118af9941c5b2
atrox3d <atrox3d@gmail.com>
2018-05-20 10:46:07 +0200

    db stuff

--- services/es-php-mysql/php/html/lib/Settings.php
+++ services/es-php-mysql/php/html/lib/Settings.php
@@ -6,28 +6,28 @@ error_reporting(E_ALL);
 
 #define('DB_HOST', 'localhost');
 #define('DB_HOST', 'elasticsearchphpmysql_mysql_1');
-define('DB_HOST', getenv( "DB_HOST" ));
+define('DB_HOST', getenv( "DB_HOST" )); # docker-compose.yml
 
 #define('DB_USER', 'root');
-define('DB_USER', getenv( "DB_USER" ));
+define('DB_USER', getenv( "DB_USER" )); # docker-compose.yml
 
 #define('DB_PASSWORD', 'girnar');
 #define('DB_PASSWORD', 'p@ssw0rd');
-define('DB_PASSWORD', getenv( "DB_PASSWORD" ));
+define('DB_PASSWORD', getenv( "DB_PASSWORD" )); # docker-compose.yml
 
 #define('DB_DATABASE', 'ecommerce');
-define('DB_DATABASE', getenv( "DB_DATABASE" ));
+define('DB_DATABASE', getenv( "DB_DATABASE" )); # docker-compose.yml
 
 
 function debug($var, $message="", $force=false, $echo=false) {
 	if( DEBUG or $force) {
-		echo "debug";
+		#echo "debug";
 		$trace=debug_backtrace();
 		if( isset( $trace[1]['function'] ))
 			$caller=$trace[1]['function'];
 		else
 			$caller="main";
-		echo "<pre>[".basename(__FILE__)."/$caller] $message";
+		echo "<pre>[DEBUG][".basename(__FILE__)."/$caller] $message: ";
 		if( $echo ) {
 			echo $var;
 		} else {
@@ -82,19 +82,26 @@ if (!$con) {
 
 function getResult($query) {
 	global $con;
+	#
 	debug($con, "\$con");
 	debug($query, "\$query");
+	#
     $query = mysqli_query($con, $query);
+	#
 	debug($query, "\$query");
+	#
     $new_array = array();
     while ($row = mysqli_fetch_assoc($query)) {
+		#
 		debug($row, "\$row");
+		#
         $new_array[] = $row;
     }
     return $new_array;
 }
 
-define('ES_HOST', getenv( "ES_HOST" ));
+
+define('ES_HOST', getenv( "ES_HOST" )); # docker-compose.yml
 define('ES_PORT', 9200);
 /*
  * get results from query 
