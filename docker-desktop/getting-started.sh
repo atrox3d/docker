08cb2287a2f90f7eb45ee8ef7b8aabf9b1b9a801
atrox3d <atrox3d@gmail.com>
2021-06-22 16:09:12 +0200

    docker-desktop/getting-started.sh

--- docker-desktop/getting-started.sh
+++ docker-desktop/getting-started.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-docker rm -f getting-started
-docker  image rm -f docker/getting-started
-docker run -d -p80:80 --name getting-started docker/getting-started
+docker ps | grep -q 'docker/getting-started' && {
+	echo "INFO| docker/getting-started is running"
+	echo "INFO| closing..."
+	docker rm -f getting-started
+}
+
+[ "${1:-UNSET}" == "--reset" ] && {
+	echo "INFO| remove image..."
+	docker  image rm -f docker/getting-started
+}
+
+HOSTPORT=80
+echo "INFO| docker run -d -p${HOSTPORT}:80 --name getting-started docker/getting-started"
+docker run -d -p${HOSTPORT}:80 --name getting-started docker/getting-started
+echo "getting-started listening on localhost:$HOSTPORT"
+
